ea7ccd5d17ff4dfb975e4ca4f9db9b64840accc1
bermetbema <bermetozgorushova02@gmail.com>
2025-12-19 22:10:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const blue = document.getElementById("blue");
+blue.style.width = "200px";
+blue.style.height = "60px";
+blue.style.backgroundColor = "#0b4cb8";
+blue.style.borderRadius = "10px";
+blue.style.margin = "20px";
+
+
+const yellow = document.getElementById("yellow");
+yellow.style.width = "80px";
+yellow.style.height = "80px";
+yellow.style.backgroundColor = "#ffc14d";
+yellow.style.borderRadius = "10px";
+yellow.style.margin = "20px";
+
+
+const red = document.getElementById("red");
+red.style.width = "80px";
+red.style.height = "80px";
+red.style.backgroundColor = "red";
+red.style.borderRadius = "50%";
+red.style.margin = "20px";
+
+
+const dom = document.getElementById("dom");
+dom.style.width = "200px";
+dom.style.height = "120px";
+dom.style.backgroundColor = "#3bb0ff";
+dom.style.color = "white";
+dom.style.fontSize = "24px";
+dom.style.fontWeight = "bold";
+dom.style.display = "flex";
+dom.style.alignItems = "center";
+dom.style.justifyContent = "center";
+dom.style.borderRadius = "10px";
+dom.style.margin = "20px";
+
+
+const img = document.getElementById("domImage");
+
+
+img.style.width = "150px";     
+img.style.height = "auto";    
+
